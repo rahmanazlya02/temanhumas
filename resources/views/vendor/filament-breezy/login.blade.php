f24f8ba8265a3f501960b655d4d6fbde3a287427
rahmanazlya02 <susantonazlya@gmail.com>
2024-12-01 17:38:50 +0700

    ubah ukuran logo di login page

--- resources/views/vendor/filament-breezy/login.blade.php
+++ resources/views/vendor/filament-breezy/login.blade.php
@@ -14,7 +14,7 @@
     </div>
 
     <h2 class="mb-0">
-        <img src="{{ asset('img/temanhumas.png') }}" class="h-20 mx-auto mb-0">
+        <img src="{{ asset('img/temanhumas.png') }}" class="h-16 mx-auto mb-0">
     </h2>
 
     @if (config('system.login_form.is_enabled'))
