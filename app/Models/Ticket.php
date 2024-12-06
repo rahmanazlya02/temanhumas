84577dd17e4f20baaa3c47b108cc6e5c64ec9d20
222212736 <222212736@stis.ac.id>
2024-12-06 20:56:14 +0700

    reminder task serta role anggota ketika buat akun

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -21,21 +21,9 @@ class Ticket extends Model implements HasMedia
     use HasFactory, SoftDeletes, InteractsWithMedia;
 
     protected $fillable = [
-        'name',
-        'content',
-        'owner_id',
-        'responsible_id',
-        'status_id',
-        'project_id',
-        'code',
-        'order',
-        'type_id',
-        'priority_id',
-        'estimation',
-        'deadline',
-        'reminder',
-        'epic_id',
-        'sprint_id'
+        'name', 'content', 'owner_id', 'responsible_id',
+        'status_id', 'project_id', 'code', 'order', 'type_id',
+        'priority_id', 'estimation', 'deadline', 'epic_id', 'sprint_id'
     ];
 
     public static function boot()
