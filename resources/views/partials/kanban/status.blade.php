541734a36c5cc39264b129f55431470e525a731a
unknown <yogaregitahamzah@gmail.com>
2024-12-07 14:01:03 +0700

    Deskripsi perubahan pada file kanban status

--- resources/views/partials/kanban/status.blade.php
+++ resources/views/partials/kanban/status.blade.php
@@ -16,27 +16,13 @@
             @include('partials.kanban.record')
         @endforeach
 
-        <!-- href="{{ route('filament.resources.tickets.create', ['project' => request()->get('project')]) }}" -->
         @if($status['add_ticket'])
             <a class="create-record hover:cursor-pointer"
-               wire:click="createTicket"
-               target="_blank">
-                <x-heroicon-o-plus class="w-4 h-4" /> {{ __('Create ticket') }}
+            href="{{ route('filament.resources.tickets.create', ['project' => request()->get('project')]) }}">
+                <x-heroicon-o-plus class="w-4 h-4" /> {{ __('Create Ticket') }}
             </a>
-
-            @if($ticket)
-                <!-- Epic modal -->
-                <div class="dialog-container">
-                    <div class="dialog dialog-xl">
-                        <div class="dialog-header">
-                            {{ __('Create ticket') }}
-                        </div>
-                        <div class="dialog-content">
-                            @livewire('road-map.issue-form', ['project' => null])
-                        </div>
-                    </div>
-                </div>
-            @endif
         @endif
+
+
     </div>
 </div>
